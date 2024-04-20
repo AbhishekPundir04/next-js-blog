f7f6f37016dda1fe9432cf259a6aa1735091659b
Abhishek Pundir <AbhishekPundir04>
2024-04-20 11:26:54 +0530

    code updated

--- src/app/homepage/page.js
+++ src/app/homepage/page.js
@@ -20,7 +20,7 @@ export default function HomePage() {
             <div className=" mx-20">
                 <span className="font-bold text-4xl text-[#183B56]">Popular Articles</span>
                 <div className="flex justify-between mt-4">
-                    <p className="text-[#5A7184] leading-8	w-100">We share common trends, strategies ideas, opinions, short & long stories from the team behind company.</p>
+                    <p className="text-[#5A7184] leading-8	w-100">We share common trends, strategies ideas, opinions, short & long stories from the team behind company</p>
                     <div className="flex items-center rounded border border-solid border-[#1565D8] px-2"><span className=" rounded text-[#1565D8] flex ">View All <Image src="arrow-right.svg" alt="view-icon" width={15} height={15} className="ml-2" /></span>  </div>
                 </div>
             </div>
